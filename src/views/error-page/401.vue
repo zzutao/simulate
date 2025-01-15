ebbc11b0a7e519045301fb1d2bc5931a1c3dac92
zzutao <3321829098@qq.com>
2025-01-15 09:28:36 +0800

    401页面

--- src/views/error-page/401.vue
+++ src/views/error-page/401.vue
@@ -10,7 +10,7 @@
         </h1>
         gif来源<a href="https://zh.airbnb.com/" target="_blank">airbnb</a> 页面
         <h2>你没有权限去该页面</h2>
-        <h6>如有不满请联系你领导</h6>
+        <!-- <h6>如有不满请联系你领导</h6> -->
         <ul class="list-unstyled">
           <li>或者你可以去:</li>
           <li class="link-type">
@@ -18,10 +18,10 @@
               回首页
             </router-link>
           </li>
-          <li class="link-type">
+          <!-- <li class="link-type">
             <a href="https://www.taobao.com/">随便看看</a>
-          </li>
-          <li><a href="#" @click.prevent="dialogVisible=true">点我看图</a></li>
+          </li> -->
+          <!-- <li><a href="#" @click.prevent="dialogVisible=true">点我看图</a></li> -->
         </ul>
       </el-col>
       <el-col :span="12">
